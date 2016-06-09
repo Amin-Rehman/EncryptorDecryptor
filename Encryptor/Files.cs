b58ca415ae0402365ff8b4c7ae9ee5f68807ee4b
Amin Ur Rehman <a.u.rehman@hotmail.co.uk>
2016-06-10 00:06:02 +0100

    Adding just a list which shows some sample data. Using IObservableCollectionView.

---
+++ Encryptor/Files.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryptor
+{
+    public class Files
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
--- Encryptor/MainWindow.xaml.cs
+++ Encryptor/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,26 @@ using System.Windows.Shapes;
 
 namespace Encryptor
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Files> files;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            files = new ObservableCollection<Files>()
+            {
+                new Files(){Name="Sample File 1",Address="Some sample path 1"},
+                new Files(){Name="Sample File 2",Address="Some sample path 2"},
+                new Files(){Name="Sample File 3",Address="Some sample path 3"}
+            };
+
+            filesInDirListView.ItemsSource = files;
         }
     }
 }
