c43d964d4516ea4b4bfc5e1525729ad86ed0d143
Amin Ur Rehman <a.u.rehman@hotmail.co.uk>
2016-06-15 22:09:49 +0100

    Adding an XOR class to XOR two strings

---
+++ SharedProject/XOR.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedProject
+{
+    public static class XOR
+    {
+        /// <summary>
+        /// Helper method to XOR two strings which are input
+        /// </summary>
+        public static string XORStrings(string a, string b)
+        {
+            int baseLength;
+
+            // a is smaller than b, pad a from left
+            if (a.Length < b.Length)
+            {
+                a = a.PadLeft(b.Length, (char)(0));
+                baseLength = b.Length;
+            }
+            else
+            {
+                b = b.PadLeft(a.Length, (char)(0));
+                baseLength = a.Length;
+            }
+
+            char[] charAArray = a.ToCharArray();
+            char[] charBArray = b.ToCharArray();
+
+            char[] result = new char[baseLength];
+
+
+            for (int i = 0; i < baseLength; i++)
+            {
+                result[i] = (char)((short)(charAArray[i]) ^ (short)(charBArray[i]));
+            }
+
+            string strResult = new string(result);
+            strResult = strResult.Trim((char)(0));
+
+            return strResult;
+
+        }
+    }
+}
