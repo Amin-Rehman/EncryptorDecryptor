88675ef6e3efba630d0374321c4f469125f1440e
Amin Ur Rehman <a.u.rehman@hotmail.co.uk>
2016-06-17 21:48:46 +0100

    Ability to generate MD5 given a file.

---
+++ SharedProject/MD5Generator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SharedProject
+{
+    class MD5Generator
+    {
+        /// <summary>
+        /// Helper method to Generate MD5 of a file
+        /// </summary>
+        public static string GetMD5HashFromFile(string fileName)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(fileName))
+                {
+                    return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", string.Empty);
+                }
+            }
+        }
+    }
+}
