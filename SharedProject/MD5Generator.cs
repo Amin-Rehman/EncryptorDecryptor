dcd63e7e5f051ce4be53e9093253fbd92c825295
Amin Ur Rehman <a.u.rehman@hotmail.co.uk>
2016-06-17 23:04:25 +0100

    Doing MD5 Generator in a task.

--- SharedProject/MD5Generator.cs
+++ SharedProject/MD5Generator.cs
@@ -3,30 +3,48 @@ using System.Collections.Generic;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Threading;
 
+/*
+Example usage:
+        private async void GenerateMD5Async()
+        {
+            string fileToTest = Directory.GetCurrentDirectory() + "\\testFile.txt";
+
+            var md5OfFile = await MD5Generator.GetMD5HashFromFile(fileToTest);
+
+            Console.WriteLine();
+        }
+*/
 namespace SharedProject
 {
     class MD5Generator
     {
         /// <summary>
-        /// Helper method to Generate MD5 of a file
+        /// Helper method to Generate MD5 of a file. This will do the file reading and MD5 Hashing in a separate task
+        /// 
         /// </summary>
-        public static string GetMD5HashFromFile(string fileName)
+        public static async Task<string> GetMD5HashFromFile(string fileName)
         {
             try
             {
-                using (var md5 = MD5.Create())
-                {
-                    using (var stream = File.OpenRead(fileName))
+                var result = await Task.Run(() => {
+                    using (var md5 = MD5.Create())
                     {
+                        var stream = File.OpenRead(fileName);
                         return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", string.Empty);
                     }
-                }
+                });
+
+                return result;
+
             }
             catch (Exception e)
             {
                 MessageBox.Show("Exception while writing JSON file: " + e.Message);
-                return;
+                return null;
             }
             
         }
