a92236e979a254f40e6a04c036935171ca46b50f
Amin Ur Rehman <a.u.rehman@hotmail.co.uk>
2016-06-18 20:30:36 +0100

    Add a GUID generator class. Easy peasy.

---
+++ SharedProject/GUIDGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedProject
+{
+    public class GUIDGenerator
+    {
+        /// <summary>
+        /// Return a brand new GUID as a String
+        /// </summary>
+        public static string getGuid()
+        {
+            var guid = Guid.NewGuid();
+            return guid.ToString();
+        }
+    }
+}
