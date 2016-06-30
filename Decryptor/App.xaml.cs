e139f856315b25ac5e1d92e567f66e2546de1efe
Amin Ur Rehman <a.u.rehman@hotmail.co.uk>
2016-06-30 20:18:30 +0100

    Adding TrueCrypt helper.

--- Decryptor/App.xaml.cs
+++ Decryptor/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using SharedProject;
 
 namespace Decryptor
 {
@@ -13,5 +14,14 @@ namespace Decryptor
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            TrueCryptHelper.StartDeviceDriver();
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            TrueCryptHelper.StopDeviceDriver();
+        }
     }
 }
---
+++ SharedProject/TrueCryptHelper.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SharedProject
+{
+    class TrueCryptHelper
+    {
+
+        [DllImport("Crypto.dll")]
+        private static extern void startDeviceDriver();
+
+        [DllImport("Crypto.dll")]
+        private static extern void stopDeviceDriver();
+
+
+        [DllImport("Crypto.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void mountContainer(int iDrive, string password, int passwordLength, string pathToContainer);
+        [DllImport("Crypto.dll")]
+        private static extern void unMountContainer();
+
+        static bool isContainerMountedCalled = false;
+
+
+        /// <summary>
+        /// Helper method to mount a container.
+        /// </summary>
+        /// <param name="password"> Password for the TrueCrypt container</param>
+        /// <param name="pathToContainer">Path to the container</param>
+        public static void MountContainer(string password, string pathToContainer)
+        {
+            int driveNumber = GetDriveToMount();
+            if (driveNumber == -1 || password.Length == 0)
+            {
+                // Error evaluating available driver to mount
+                return;
+            }
+
+            mountContainer(driveNumber, password, password.Length, pathToContainer);
+            isContainerMountedCalled = true;
+
+        }
+
+        /// <summary>
+        /// Unmount the container if one is mounted.
+        /// </summary>
+        public static void UnMountContainer()
+        {
+            if (isContainerMountedCalled == true)
+            {
+                // Display a message box asking users if they
+                // want to exit the application.
+                if (MessageBox.Show("Any open instances of Windows Explorer will have to close. Do you wish to continue?",
+                "Closing Windows Explorer",
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    foreach (Process p in Process.GetProcessesByName("explorer"))
+                    {
+                        p.Kill();
+                    }
+
+                    // Wait here till all the explorer processes have ended
+                    DateTime startTime = DateTime.Now;
+
+                    while (true)
+                    {
+                        DateTime currentTime = DateTime.Now;
+                        var timeDiff = currentTime.Subtract(startTime);
+
+                        // Break the check if its been more than 5 seconds
+                        if (timeDiff.Seconds > 5)
+                        {
+                             break;
+                        }
+                    }
+                    unMountContainer();
+                    isContainerMountedCalled = false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Start TrueCrypt drivers. Ideally call this on App Startup
+        /// </summary>
+        public static void StartDeviceDriver()
+        {
+            startDeviceDriver();
+        }
+
+        /// <summary>
+        /// Stop TrueCrypt drivers. Ideally call this on App Exit
+        /// </summary>
+        public static void StopDeviceDriver()
+        {
+            stopDeviceDriver();
+        }
+
+        /// <summary>
+        /// Helper Method to iterate through all the drives and returns the drive integer for the TrueCrypt container
+        /// </summary>
+        /// <returns>
+        /// Integer of the drive number which can be mounted
+        /// </returns>
+        private static int GetDriveToMount()
+        {
+            const int ASCII_OFFSET = 65;
+            int driveToMount = -1;
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            List<char> driveLetters = new List<char>();
+            List<int> trueCryptDriveLetter = new List<int>();
+
+
+            foreach (DriveInfo d in allDrives)
+            {
+                // Retrieve just the drive letter
+                char filteredName = d.Name.ElementAt<char>(0);
+                driveLetters.Add(filteredName);
+            }
+
+            foreach (char driveChar in driveLetters)
+            {
+                trueCryptDriveLetter.Add((int)driveChar - ASCII_OFFSET);
+            }
+
+            // Chose random drive which is available
+            // Chose a number between 0 and 25 excluding the drives already in use
+            Random random = new Random();
+            do
+            {
+                driveToMount = random.Next(0, 25);
+            } while (trueCryptDriveLetter.Contains(driveToMount));
+
+            return driveToMount;
+        }
+
+
+    }
+}
