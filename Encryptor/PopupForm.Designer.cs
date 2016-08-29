1f453e85ed040ceeb4030b8fe09269235654161e
Amin Ur Rehman <a.u.rehman@hotmail.co.uk>
2016-08-29 16:53:26 +0100

    Initial working implementation of DVD Writing

--- Encryptor/PopupForm.Designer.cs
+++ Encryptor/PopupForm.Designer.cs
@@ -38,17 +38,19 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(295, 13);
+            this.label1.Size = new System.Drawing.Size(367, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Please enter password which will be used to encrypt the data";
             // 
             // passwordTextBox
             // 
-            this.passwordTextBox.Location = new System.Drawing.Point(15, 39);
+            this.passwordTextBox.Location = new System.Drawing.Point(20, 48);
+            this.passwordTextBox.Margin = new System.Windows.Forms.Padding(4);
             this.passwordTextBox.Name = "passwordTextBox";
-            this.passwordTextBox.Size = new System.Drawing.Size(358, 20);
+            this.passwordTextBox.Size = new System.Drawing.Size(476, 22);
             this.passwordTextBox.TabIndex = 1;
             this.passwordTextBox.UseSystemPasswordChar = true;
             this.passwordTextBox.TextChanged += new System.EventHandler(this.passwordTextBox_TextChanged);
@@ -56,9 +58,10 @@
             // OKButton
             // 
             this.OKButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.OKButton.Location = new System.Drawing.Point(298, 89);
+            this.OKButton.Location = new System.Drawing.Point(397, 110);
+            this.OKButton.Margin = new System.Windows.Forms.Padding(4);
             this.OKButton.Name = "OKButton";
-            this.OKButton.Size = new System.Drawing.Size(75, 23);
+            this.OKButton.Size = new System.Drawing.Size(100, 28);
             this.OKButton.TabIndex = 2;
             this.OKButton.Text = "OK";
             this.OKButton.UseVisualStyleBackColor = true;
@@ -67,9 +70,10 @@
             // CrossButton
             // 
             this.CrossButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.CrossButton.Location = new System.Drawing.Point(15, 89);
+            this.CrossButton.Location = new System.Drawing.Point(20, 110);
+            this.CrossButton.Margin = new System.Windows.Forms.Padding(4);
             this.CrossButton.Name = "CrossButton";
-            this.CrossButton.Size = new System.Drawing.Size(75, 23);
+            this.CrossButton.Size = new System.Drawing.Size(100, 28);
             this.CrossButton.TabIndex = 3;
             this.CrossButton.Text = "Cancel";
             this.CrossButton.UseVisualStyleBackColor = true;
@@ -77,9 +81,9 @@
             // 
             // PopupForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(385, 124);
+            this.ClientSize = new System.Drawing.Size(525, 168);
             this.ControlBox = false;
             this.Controls.Add(this.CrossButton);
             this.Controls.Add(this.OKButton);
@@ -87,6 +91,7 @@
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "PopupForm";
